a4641255c2d329c2a15a2c28174a3484ed3c0da5
Schmiddi-85 <m.a.schmid@gmx.de>
2025-11-13 21:41:42 +0100

    Update machines.ts

--- api/machines.ts
+++ api/machines.ts
@@ -10,8 +10,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
       error: "Missing PRINTOS_KEY or PRINTOS_SECRET",
     });
   }
-
-  const BASE_URL = "https://printos.api.hp.com/printbeat";
+const BASE_URL = "https://printos.api.hp.com/api/PrintbeatService";
   const PATH = "/externalApi/machines";
 
   const method = "GET";
